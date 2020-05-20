4f75f00b8c9eb494fabb7209519b80dc120617ef
luis.forero <luis.forero@wnco.com>
2020-05-20 16:01:17 -0500

    [USER-ROLES AND AUTHORITIES] - Application users - In memory user detail Manager - Password Encoding with BCRYPT

--- src/main/java/com/example/postspringsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/postspringsecurity/security/ApplicationSecurityConfig.java
@@ -1,22 +1,57 @@
 package com.example.postspringsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "index", "/css/*","/js/*")
+                .antMatchers("/", "index", "/css/*", "/js/*")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        User.UserBuilder luwi = User.builder()
+                .username("luwifor")
+                .password(passwordEncoder.encode("password"))
+                .roles("VIEWER");//ROLE VIEWER
+        UserDetails katya = User.builder()
+                .username("katya")
+                .password(passwordEncoder.encode("sirena"))
+                .roles("AUTHOR")
+                .build();
+        UserDetails ludwig = User.builder()
+                .username("ludwig")
+                .password(passwordEncoder.encode("symphony"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(luwi.build(), katya, ludwig);
     }
 }
