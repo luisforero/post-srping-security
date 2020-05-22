26a3a8f1205ad4a7e9245c50fe2884de7f103cac
luis.forero <luis.forero@wnco.com>
2020-05-22 18:30:02 -0500

    Adding login and logout configurations.

---
+++ src/main/java/com/example/postspringsecurity/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package com.example.postspringsecurity.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+    @GetMapping("login")
+    public String getLoginView(){
+        return "login";
+    }
+
+}
